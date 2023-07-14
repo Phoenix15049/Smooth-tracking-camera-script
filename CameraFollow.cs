d5f05a6a4f3b9724be1fe3ed44c8be00d94e7024
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-07-14 18:13:45 +0330

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	[SerializeField] private Vector3 offset;
+	[SerializeField] private float smoothness = 0.3f;
+	private Vector3 cameraVelocity = Vector3.zero; 
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void LateUpdate()
+	{
+		Vector3 targetposition = target.position + offset;
+		transform.position = Vector3.SmoothDamp(transform.position, targetposition, ref cameraVelocity, smoothness);
+		transform.LookAt(target);
+
+
+    }
+}
